bbd124cbdb74e49e21c656ced3b417ff64514800
gearup2000 <54724126+gearup2000@users.noreply.github.com>
2022-05-20 01:20:39 +0300

    Added App.js and index.js

---
+++ part1/1.4/App.js
@@ -0,0 +1,72 @@
+//Part 1.4
+
+export const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React:",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data:",
+        exercises: 7
+      },
+      {
+        name: "State of a component:",
+        exercises: 14
+      }
+    ]
+  }
+
+  const Header = () => {
+    return (
+      <div>
+        <h1>{course.name}</h1>
+      </div>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part eachpart={course.parts[0]} />
+        <Part eachpart={course.parts[1]} />
+        <Part eachpart={course.parts[2]} />
+      </div>
+    )
+  }
+
+  const Part = props => {
+    const { eachpart } = props
+    return (
+      <div>
+        <p>
+          {eachpart.name} {eachpart.exercises}
+        </p>
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div>
+        <p>
+          Number of exercises: {course.parts[0].exercises + 
+          course.parts[1].exercises + 
+          course.parts[2].exercises}
+        </p>
+      </div>
+      )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App
