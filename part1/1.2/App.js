4c9db47242fdc8f2aff7312eca4388892baab7d6
gearup2000 <54724126+gearup2000@users.noreply.github.com>
2022-05-19 20:45:28 +0300

    const Total-fixed typos of course.parts[0].exercises

--- part1/1.2/App.js
+++ part1/1.2/App.js
@@ -56,8 +56,8 @@ export const App = () => {
       <div>
         <p>
           Number of exercises: {course.parts[0].exercises + 
-          course.parts[0].exercises + 
-          course.parts[0].exercises}
+          course.parts[1].exercises + 
+          course.parts[2].exercises}
         </p>
       </div>
       )
