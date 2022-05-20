dce9aba49ad837ed755dfcaddd41b8a997c7b17e
gearup2000 <54724126+gearup2000@users.noreply.github.com>
2022-05-21 01:40:02 +0300

    Redone versionversion of App.js uploaded

---
+++ part1/1.4/App.js
@@ -0,0 +1,61 @@
+//Part 1.4
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const newContent = props.parts.map(function(item) {
+    return (
+      <div>
+        <p> {item.name} {item.exercises}</p>
+      </div>
+    )
+  })
+  
+  return newContent
+}
+
+const Total = (props) => {
+  let all = 0
+    const newContent = props.parts.map(function (item) 
+      {all = all + item.exercises})
+  
+  return (
+    <div>
+      <p>Number of exercises {all} </p>
+    </div>
+  )
+}
+
+export default App
