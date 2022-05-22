29b1eda2ece3b7611a26ff9fd4d71ff0c11c04bd
gearup2000 <54724126+gearup2000@users.noreply.github.com>
2022-05-22 19:28:29 +0300

    Add files via upload

---
+++ Event Handler v2/App.js
@@ -0,0 +1,56 @@
+//Event handler 2 
+
+import { useState } from 'react';
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}> {text} </button>
+)
+
+  const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+
+  )
+}
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  )
+}
+
+export default App
